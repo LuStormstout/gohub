b38b3584e1ca2017286e7ae15a3dd84ab91bb2b1
Lu Stormstout <jacklu.net@gmail.com>
2023-12-18 22:12:26 +0800

    Integrate Gin framework

--- main.go
+++ main.go
@@ -3,17 +3,41 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strings"
 )
 
 func main() {
-	gin.SetMode(gin.DebugMode)
 
-	r := gin.Default()
+	// new a gin engine
+	r := gin.New()
+
+	// use middleware
+	r.Use(gin.Logger(), gin.Recovery())
+
+	// add a route
 	r.GET("/", func(c *gin.Context) {
+		// return a json
 		c.JSON(http.StatusOK, gin.H{
 			"test": "Hello Gohub!",
 		})
 	})
 
-	_ = r.Run(":8080")
+	// 404 handler
+	r.NoRoute(func(c *gin.Context) {
+		// check accept header
+		acceptString := c.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			// return a string
+			c.String(http.StatusNotFound, "404 Not Found")
+		} else {
+			// default return a json
+			c.JSON(http.StatusNotFound, gin.H{
+				"code": http.StatusNotFound,
+				"msg":  "404 Not Found",
+			})
+		}
+	})
+
+	// run
+	_ = r.Run(":8000")
 }
